4d95544e9d5ce2dcde288a5a3295aef1e32599b8
clemenvidal <vidalclementina1@gmail.com>
2022-09-29 17:40:41 -0300

    Swagger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import app from "./app.js";
 import { createAdminUser } from "./libs/createUser.js";
 import "./database.js";
+import express from "express";
+import indexRoutes from "./routes/index.routes.js";
+import notesRoutes from "./routes/notes.routes.js";
+import userRoutes from "./routes/auth.routes.js";
+import "./config/passport.js";
+//middleware
+
+
+//swagger
+
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+const swaggerSpec = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node MongoDB API",
+      version: "1.0.0"
+    },
+    servers: [
+        {
+          url:"http://localhost:4200"
+        }
+    ]
+  },
+  apis: [`${path.join(__dirname,"./routes/*.js",import.meta.url)}`],
+}
+app.use(express.json());
+app.use("/api",userRoutes);
+app.use("/api-doc",swaggerUI.serve,swaggerUI.setup(swaggerJsDoc(swaggerSpec)));
+
+
 
 async function main() {
   await createAdminUser();
