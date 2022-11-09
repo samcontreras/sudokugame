50e9cfa6d532e3f08fac2b1f380274dd43c6cfbe
Samuel Contreras Revollo <57951231+samcontreras@users.noreply.github.com>
2022-11-08 23:49:12 -0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-import { config } from "dotenv";
-config();
-
-export const PORT = process.env.PORT || 4200;
-export const MONGODB_URI = "mongodb+srv://Clemen:45eoYCymZMkDJBli@cluster0.5ycrszm.mongodb.net/test";
