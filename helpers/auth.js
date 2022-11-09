f2dc5aa6509ca16dab202071efb70f0ef4d8d9d7
Samuel Contreras Revollo <57951231+samcontreras@users.noreply.github.com>
2022-11-08 23:45:43 -0300

    Delete helpers directory

--- helpers/auth.js
+++
@@ -1,7 +0,0 @@
-export const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  req.flash("error_msg", "No autorizado");
-  res.redirect("/users/signin");
-};
